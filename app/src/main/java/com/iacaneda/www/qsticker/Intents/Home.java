41c04e35fcb45895bcd02a5e17f4b342d02f5216
Isabelle Caneda <Isabelle Caneda>
2017-04-26 04:01:38 -0400

    Initial commit

---
+++ app/src/main/java/com/iacaneda/www/qsticker/Intents/Home.java
@@ -0,0 +1,21 @@
+package com.iacaneda.www.qsticker.Intents;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.iacaneda.www.qsticker.LoginActivity;
+import com.iacaneda.www.qsticker.R;
+
+/**
+ * Created by Isabelle on 4/26/2017.
+ */
+
+public class Home extends LoginActivity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+}
